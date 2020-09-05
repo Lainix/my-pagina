370c78429d8d71dfa83640d3b9123ee18d0457f8
Lainix <linkelin08@hotmail.com>
2020-09-04 22:00:53 -0500

    Updates

---
+++ precache-manifest.e93acb18806d0854d98b8ccede107fa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2c5823c6df7565d440ccf3dc929401",
+    "url": "/my-pagina/index.html"
+  },
+  {
+    "revision": "7ca7f1bf6b03bb090e13",
+    "url": "/my-pagina/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b80f807efe58b985e8ef",
+    "url": "/my-pagina/static/js/2.191148a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-pagina/static/js/2.191148a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca7f1bf6b03bb090e13",
+    "url": "/my-pagina/static/js/main.28d3cfa7.chunk.js"
+  },
+  {
+    "revision": "4482eeb8af93fb8d3b2d",
+    "url": "/my-pagina/static/js/runtime-main.d855da23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-pagina/static/media/logo.5d5d9eef.svg"
+  }
+]);
